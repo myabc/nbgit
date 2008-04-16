d0ca0a6aa0dc4bf50ca3616e0745d22fc856c17d
Alex Coles <alexbcoles@mac.com>
2008-04-16 12:23:09 +0200

    Adding missing META-INF directory: NBGit should now run properly and appear in menus. Updated spec versions, copyright, whitespace changes.

--- src/org/netbeans/modules/git/FileStatusCache.java
+++ src/org/netbeans/modules/git/FileStatusCache.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2008 Sun
  * Microsystems, Inc. All Rights Reserved.
  * Portions Copyright 2008 Alexander Coles (Ikonoklastik Productions).
  *

fdb55381406a77dbee6db1e4a87016604e06cadd
Jonas Fonseca <fonseca@diku.dk>
2009-08-06 23:10:25 -0400

    Remove unused code in Excludes
    Signed-off-by: Jonas Fonseca <fonseca@diku.dk>

--- src/org/nbgit/util/exclude/Excludes.java
+++ src/org/nbgit/util/exclude/Excludes.java
@@ -42,28 +42,17 @@
 package org.nbgit.util.exclude;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.util.Collections;
-import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.Vector;
-import java.util.logging.Level;
-import org.nbgit.StatusCache;
 import org.nbgit.Git;
 import org.netbeans.api.project.ProjectManager;
 import org.netbeans.api.queries.SharabilityQuery;
-import org.openide.filesystems.FileLock;
-import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
 import org.spearce.jgit.lib.Repository;
 
@@ -77,25 +66,8 @@ public class Excludes {
     private Excludes() {
     }
 
-    // IGNORE SUPPORT GIT: following file patterns are added to {Git repos}/.gitignore and Git will ignore any files
-    // that match these patterns, reporting "I"status for them // NOI18N
-    //private static final String [] GIT_IGNORE_FILES = { ".orig", "\\.orig\\..*$", "\\.chg\\..*$", ".rej", "\\.conflict\\~$"}; // NOI18N
-    private static final String[] GIT_IGNORE_FILES = {".orig"};
-    private static final String GIT_IGNORE_ORIG_FILES = "\\.orig$"; // NOI18N
-    private static final String GIT_IGNORE_ORIG_ANY_FILES = "\\.orig\\..*$"; // NOI18N
-    private static final String GIT_IGNORE_CHG_ANY_FILES = "\\.chg\\..*$"; // NOI18N
-    private static final String GIT_IGNORE_REJ_ANY_FILES = "\\.rej$"; // NOI18N
-    private static final String GIT_IGNORE_CONFLICT_ANY_FILES = "\\.conflict\\~$"; // NOI18N
     private static final String FILENAME_GITIGNORE = ".gitignore"; // NOI18N
     private static HashMap<String, List<PathPattern>> ignorePatterns;
-
-    private static void resetIgnorePatterns(File file) {
-        if (ignorePatterns == null) {
-            return;
-        }
-        String key = file.getAbsolutePath();
-        ignorePatterns.remove(key);
-    }
 
     private static List<PathPattern> getIgnorePatterns(File file) {
         if (ignorePatterns == null) {
@@ -133,12 +105,6 @@ public class Excludes {
     }
 
     public static boolean isIgnored(File file, boolean checkSharability) {
-        // FIXME Disabled for now.
-        /*
-        if (true != false) {
-            return false;
-        }
-         */
         if (file == null) {
             return false;
         }
@@ -209,147 +175,6 @@ public class Excludes {
         return null;
     }
 
-    /**
-     * Create a .gitignore file in the repository in which the given
-     * file belongs. The .gitignore file will contain the ignore
-     * patterns specified by {@link #GIT_IGNORE_FILES}.
-     *
-     * @param path to repository to place .gitignore file
-     */
-    /*
-    public static void createIgnored(File path, String ignorePatterns) {
-        if (path == null) {
-            return;
-        }
-        BufferedWriter fileWriter = null;
-        Git git = Git.getInstance();
-        File root = git.getTopmostManagedParent(path);
-        if (root == null) {
-            return;
-        }
-        File ignore = new File(root, FILENAME_GITIGNORE);
-
-        try {
-            if (!ignore.exists()) {
-                fileWriter = new BufferedWriter(
-                        new OutputStreamWriter(new FileOutputStream(ignore)));
-                for (String name : GIT_IGNORE_FILES) {
-                    fileWriter.write(name + "\n"); // NOI18N
-                }
-            } else {
-                addToExistingIgnoredFile(ignore);
-            }
-        } catch (IOException ex) {
-            Git.LOG.log(Level.FINE, "createIgnored(): File {0} - {1}", // NOI18N
-                    new Object[]{ignore.getAbsolutePath(), ex.toString()});
-        } finally {
-            try {
-                if (fileWriter != null) {
-                    fileWriter.close();
-                }
-                git.getStatusCache().refresh(ignore, StatusCache.REPOSITORY_STATUS_UNKNOWN);
-            } catch (IOException ex) {
-                Git.LOG.log(Level.FINE, "createIgnored(): File {0} - {1}", // NOI18N
-                        new Object[]{ignore.getAbsolutePath(), ex.toString()});
-            }
-        }
-    }
-    private static int GIT_NUM_PATTERNS_TO_CHECK = 5;
-
-    private static void addToExistingIgnoredFile(File gitIgnoreFile) {
-        if (gitIgnoreFile == null || !gitIgnoreFile.exists() || !gitIgnoreFile.canWrite()) {
-            return;
-        }
-        File tempFile = null;
-        BufferedReader br = null;
-        PrintWriter pw = null;
-        boolean bOrigAnyPresent = false;
-        boolean bOrigPresent = false;
-        boolean bChgAnyPresent = false;
-        boolean bRejAnyPresent = false;
-        boolean bConflictAnyPresent = false;
-
-        // If new patterns are added to GIT_IGNORE_FILES, following code needs to
-        // check for these new patterns
-        assert (GIT_IGNORE_FILES.length == GIT_NUM_PATTERNS_TO_CHECK);
-
-        try {
-            tempFile = new File(gitIgnoreFile.getAbsolutePath() + ".tmp"); // NOI18N
-            if (tempFile == null) {
-                return;
-            }
-            br = new BufferedReader(new FileReader(gitIgnoreFile));
-            pw = new PrintWriter(new FileWriter(tempFile));
-
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                if (!bOrigAnyPresent && line.equals(GIT_IGNORE_ORIG_ANY_FILES)) {
-                    bOrigAnyPresent = true;
-                } else if (!bOrigPresent && line.equals(GIT_IGNORE_ORIG_FILES)) {
-                    bOrigPresent = true;
-                } else if (!bChgAnyPresent && line.equals(GIT_IGNORE_CHG_ANY_FILES)) {
-                    bChgAnyPresent = true;
-                } else if (!bRejAnyPresent && line.equals(GIT_IGNORE_REJ_ANY_FILES)) {
-                    bRejAnyPresent = true;
-                } else if (!bConflictAnyPresent && line.equals(GIT_IGNORE_CONFLICT_ANY_FILES)) {
-                    bConflictAnyPresent = true;
-                }
-                pw.println(line);
-                pw.flush();
-            }
-            // If not found add as required
-            if (!bOrigAnyPresent) {
-                pw.println(GIT_IGNORE_ORIG_ANY_FILES);
-                pw.flush();
-            }
-            if (!bOrigPresent) {
-                pw.println(GIT_IGNORE_ORIG_FILES);
-                pw.flush();
-            }
-            if (!bChgAnyPresent) {
-                pw.println(GIT_IGNORE_CHG_ANY_FILES);
-                pw.flush();
-            }
-            if (!bRejAnyPresent) {
-                pw.println(GIT_IGNORE_REJ_ANY_FILES);
-                pw.flush();
-            }
-            if (!bConflictAnyPresent) {
-                pw.println(GIT_IGNORE_CONFLICT_ANY_FILES);
-                pw.flush();
-            }
-
-        } catch (IOException ex) {
-            // Ignore
-        } finally {
-            try {
-                if (pw != null) {
-                    pw.close();
-                }
-                if (br != null) {
-                    br.close();
-                }
-                boolean bAnyAdditions = !bOrigAnyPresent || !bOrigPresent ||
-                        !bChgAnyPresent || !bRejAnyPresent || !bConflictAnyPresent;
-                if (bAnyAdditions) {
-                    if (!GitUtils.confirmDialog(GitUtils.class, "MSG_IGNORE_FILES_TITLE", "MSG_IGNORE_FILES")) { // NOI18N
-                        tempFile.delete();
-                        return;
-                    }
-                    if (tempFile != null && tempFile.isFile() && tempFile.canWrite() && gitIgnoreFile != null) {
-                        gitIgnoreFile.delete();
-                        tempFile.renameTo(gitIgnoreFile);
-                    }
-                } else {
-                    tempFile.delete();
-                }
-            } catch (IOException ex) {
-                // Ignore
-            }
-        }
-    }
-     */
-
     private static List<PathPattern> readIgnorePatterns(File gitIgnore) {
 		Vector<PathPattern> patterns = new Vector<PathPattern>(5);
         Set<String> shPatterns;
@@ -395,77 +220,4 @@ public class Excludes {
         return entries;
     }
 
-    /*
-    private static String computePatternToIgnore(File directory, File file) {
-        String name = file.getAbsolutePath().substring(directory.getAbsolutePath().length() + 1);
-        return name.replace(' ', '?').replace(File.separatorChar, '/');
-    }
-
-    private static void writeIgnoreEntries(File directory, Set<String> entries) throws IOException {
-        File gitIgnore = new File(directory, FILENAME_GITIGNORE);
-        FileObject fo = FileUtil.toFileObject(gitIgnore);
-
-        if (entries.size() == 0) {
-            if (fo != null) {
-                fo.delete();
-            }
-            return;
-        }
-
-        if (fo == null || !fo.isValid()) {
-            fo = FileUtil.toFileObject(directory);
-            fo = fo.createData(FILENAME_GITIGNORE);
-        }
-        FileLock lock = fo.lock();
-        PrintWriter w = null;
-        try {
-            w = new PrintWriter(fo.getOutputStream(lock));
-            for (String entry : entries) {
-                w.println(entry);
-            }
-        } finally {
-            lock.releaseLock();
-            if (w != null) {
-                w.close();
-            }
-            resetIgnorePatterns(directory);
-        }
-    }
-     */
-
-    /**
-     * addIgnored - Add the specified files to the .gitignore file in the
-     * specified repository.
-     *
-     * @param directory for repository for .gitignore file
-     * @param files an array of Files to be added
-     */
-    /*
-    public static void addIgnored(File directory, File[] files) throws IOException {
-        Set<String> entries = readIgnoreEntries(directory);
-        for (File file : files) {
-            String patterntoIgnore = computePatternToIgnore(directory, file);
-            entries.add(patterntoIgnore);
-        }
-        writeIgnoreEntries(directory, entries);
-    }
-    */
-
-    /**
-     * removeIgnored - Remove the specified files from the .gitignore file in
-     * the specified repository.
-     *
-     * @param directory for repository for .gitignore file
-     * @param files an array of Files to be removed
-     */
-    /*
-    public static void removeIgnored(File directory, File[] files) throws IOException {
-        Set<String> entries = readIgnoreEntries(directory);
-        for (File file : files) {
-            String patterntoIgnore = computePatternToIgnore(directory, file);
-            entries.remove(patterntoIgnore);
-        }
-        writeIgnoreEntries(directory, entries);
-    }
-    */
 }
